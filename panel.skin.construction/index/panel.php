a3edb224749e5b788d2406528737f4232cec843d
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-07-31 11:00:01 +0700

    Updated for Panel 2.7.x

--- panel.skin.construction/index/panel.php
+++ panel.skin.construction/index/panel.php
@@ -1,23 +1,12 @@
 <?php
 
-// Disable the automatic syntax highlighter theme loader
-State::let('x.highlight.skin');
-
 $_['is']['dark'] = false;
 $_['is']['light'] = true;
 
-$_['not']['dark'] = true;
-$_['not']['light'] = false;
-
-// Add this theme
-$_['asset']['panel.skin.construction'] = [
-    'path' => stream_resolve_include_path(__DIR__ . DS . '..' . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'index' . (defined('DEBUG') && DEBUG ? '.' : '.min.') . 'css'),
-    'stack' => 20.1
-];
-
-// Remove the default theme
-$_['asset']['panel.skin.dark'] = [
-    'skip' => true
+// Add this skin
+$_['skin']['construction'] = [
+    'title' => 'Construction',
+    'path' => stream_resolve_include_path(__DIR__ . DS . '..' . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'index' . (defined('DEBUG') && DEBUG ? '.' : '.min.') . 'css')
 ];
 
 // Add `is:main` class to the folder and link menu
