550755858084b4801fd4db0b0d5eeed2a4d3d93e
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-11-12 20:43:51 +0700

    Update

--- panel.skin.construction/index/panel.php
+++ panel.skin.construction/index/panel.php
@@ -1,14 +1,30 @@
 <?php
 
-$_['is']['dark'] = false;
-$_['is']['light'] = true;
-
-// Add this skin
-$_['skin']['construction'] = [
-    'title' => 'Construction',
-    'path' => stream_resolve_include_path(__DIR__ . DS . '..' . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'index' . (defined('DEBUG') && DEBUG ? '.' : '.min.') . 'css')
-];
-
 // Add `is:main` class to the folder and link menu
 $_['lot']['bar']['lot'][0]['lot']['folder']['tags']['is:main'] = true;
 $_['lot']['bar']['lot'][0]['lot']['link']['tags']['is:main'] = true;
+
+$skin = $state->x->panel->skin ?? 'construction';
+
+// Add `construction` skin option
+if ('.state' === $_['path'] && 'g' === $_['task']) {
+    $_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['panel']['lot']['fields']['lot']['skin'] = [
+        'name' => 'state[x][panel][skin]',
+        'type' => 'option',
+        'value' => $skin,
+        'lot' => [
+            'construction' => 'Construction'
+        ],
+        'stack' => 30
+    ];
+}
+
+// Load asset if current `skin` value is `construction`
+if ('construction' === $skin) {
+    $_['asset']['panel.skin.' . $skin] = [
+        'id' => false,
+        'path' => stream_resolve_include_path(__DIR__ . DS . '..' . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'index' . (defined('DEBUG') && DEBUG ? '.' : '.min.') . 'css'),
+        'stack' => 30
+    ];
+    $_['is']['light'] = true;
+}
